3768c3626655b409c060d8a27dd7922aa9b36911
pnichols789 <pnichols789@gmail.com>
2022-10-20 20:16:48 -0500

    -

--- react-router-dom/src/App.js
+++ react-router-dom/src/App.js
@@ -1,11 +1,26 @@
-import Navigation from "./components/Navigation"
+import Navigation from "./components/Navigation"; 
+import { Routes, Route } from 'react-router-dom'
+
+import {Home, About, People, Login, SignUp, NotFound} from './pages'
 
 const App = () => {
-    return (
-      <div>
-        <Navigation/>
-      </div>
-    )
-  }
+  return (
   
-  export default App
+    <>
+      <Navigation />
+
+      <Routes>
+        <Route path="/Home" element={ <Home /> } />
+        <Route path="/about" element={<About/> } />
+        <Route path="/login" element={<Login/> } />
+        <Route path="/sign-up" element={<SignUp/> } />
+        <Route path="/people" element={ <People /> } />
+        <Route path="*" element={ <NotFound /> } />
+        
+      </Routes>
+
+    </>
+     )
+}
+
+export default App
---
+++ react-router-dom/src/pages/index.jsx
@@ -0,0 +1,9 @@
+import About from "./About";
+import Home from "./Home";
+import Login from "./Login";
+import NotFound from "./NotFound";
+import Profile from "./Profile";
+import SignUp from "./SignUp";
+import People from "./People";
+
+export { About, Home, Login, NotFound, People, Profile, SignUp}
