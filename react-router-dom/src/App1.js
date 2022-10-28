cdc40c46d86b0a4da01321520d1730d520d5483b
pnichols789 <pnichols789@gmail.com>
2022-10-28 11:01:15 -0500

    -

--- react-router-dom/src/App1.js
+++ react-router-dom/src/App1.js
@@ -1,20 +1,20 @@
-import { Routes, Route} from 'react-router-dom'
-import Navbar from './components/Navbar';
-import Posts from './components/Posts';
-import Post from './components/Post'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Posts from "./components/Posts";
+import Post from "./components/Post";
 
 function App() {
-  return (
+	return (
 		<>
 			<Navbar />
-      <Routes>   
+			<Routes>
 				<Route path="/" element={<h1>Home Page</h1>} />
 				<Route path="/home" element={<h1>Home Page</h1>} />
 				<Route path="/about" element={<h1>About Page</h1>} />
 				<Route path="/contact" element={<h1>Contact Page</h1>} />
-        <Route path="/posts" element={ <Posts /> } />
-        <Route path="/posts/:postId" element={<Post/> } />
-        <Route path="*" element={ <h1> 404 : Page Not found</h1> } />
+				<Route path="/posts" element={<Posts />} />
+				<Route path="/posts/:postId" element={<Post />} />
+				<Route path="*" element={<h1> 404 : Page Not found</h1>} />
 			</Routes>
 		</>
 	);
